735c286b2bf6131063e77200b4bece2015f9e6d4
chubaknek <dodgeandchase@outlook.com>
2019-11-13 06:28:11 +0800

    notifications with javscript toastr

--- Gemfile
+++ Gemfile
@@ -57,3 +57,4 @@ gem 'devise'
 
 
 
+
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,11 +11,23 @@ require.context('../images', true)
 
 import "bootstrap"
 import "../stylesheets/application"
+// import "toastr"
 
 document.addEventListener("turbolinks:load", () => {
   $('[data-toggle="tooltip"]').tooltip()
   $('[data-toggle="popover"]').popover()
 })
+
+
+import toastr from 'toastr';
+
+// toastr.options = {
+//     "closeButton": true
+//     .... add options here ...
+// };
+
+global.toastr = toastr;
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
