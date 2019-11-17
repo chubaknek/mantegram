33d85ab8cbc31ade802dca323a63ffe7d213d452
chubaknek <dodgeandchase@outlook.com>
2019-11-18 06:50:04 +0800

    twitter login, edit profile page, sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ protect_from_forgery with: :exception
 
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-		devise_parameter_sanitizer.permit(:account_update, keys: [:name])	
+		devise_parameter_sanitizer.permit(:account_update, keys: [:name, :website, :bio])	
 	end
 end
---
+++ db/migrate/20191117192124_add_fields_to_users.rb
@@ -0,0 +1,6 @@
+class AddFieldsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :website, :string
+    add_column :users, :bio, :string
+  end
+end
